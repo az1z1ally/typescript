b4b2029d5b829121add529f0d8226c9f427a1c4d
az1z1ally <albyally5@gmail.com>
2024-04-14 19:29:42 +0300

    covered some few types including interfaces

--- MonsterLesson/src/main.ts
+++ MonsterLesson/src/main.ts
@@ -1,2 +1,30 @@
-const a = "1"
+// VARIABLES
+const a:string = "1"
 console.log("aaa", a)
+
+// FUNCTIONS
+const getFullName = (name:string, surname:string): string => {
+    return `${name} ${surname}`
+}
+
+// OBJECTS
+// Unlike JS, TS can be helpful with entity definition 
+const user: {name:string, age:number} = { 
+    name: "TS",
+    age: 7
+}
+
+// INTERFACES -- Special type in TS that help us to describe entities(create object with some properties)
+interface User {
+    name: string
+    age?: number //age is not mandatory(?)
+}
+
+const newUser: User = {
+    name: "Interface",
+    age: 0
+}
+
+const noAgeUser: User = {
+    name: "No Age"
+}
